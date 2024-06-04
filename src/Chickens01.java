0cd75cf05d4f00112b43df8422f02c3e31970127
Aluno <Aluno@10.85.198.42>
2024-06-04 16:53:37 -0300

    java project Chickens01

---
+++ src/Chickens01.java
@@ -0,0 +1,15 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int count=15; //quantidade de ovos
+        int a, b, c; //segunda,terça, quarta
+        a = count; //segunda
+        b = count + 5; //terça
+        c = count - 5; //quarta
+        
+        System.out.println("Segnda-feira" + " " + a + ". ");
+        System.out.println("Terca-feira" + " " + b + ". ");
+        System.out.println("Quarta-feira" + " " + c + ". ");
+        
+    }   
+}
